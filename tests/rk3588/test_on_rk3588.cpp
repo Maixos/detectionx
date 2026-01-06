9b1eb06bdf86a03cd01733a3ab9a4d3f9d784b8e
Minco <903444601@qq.com>
2026-01-06 23:05:32 +0800

    Update test_on_rk3588.cpp

--- tests/rk3588/test_on_rk3588.cpp
+++ tests/rk3588/test_on_rk3588.cpp
@@ -39,7 +39,10 @@ int main(int argc, char* argv[]) {
     /// ----------------------------------------
     const int num_workers = g_config.detection_config_.num_workers;
     const std::string model_path = g_config.detection_config_.model_path;
-    const auto detector = inferencex::detection::YOLO11Engine::create(model_path, num_workers);
+    const float threshold = g_config.detection_config_.threshold;
+    const auto detector = inferencex::detection::YOLO11Engine::create(
+        model_path, threshold, num_workers
+    );
     if (!detector) {
         LOG_ERROR("test", "create engine failed");
         return -1;
